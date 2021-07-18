a9c540516c65c83e56c2a7a81413211cd8a8fe05
Collins Kanyiri <collins.kanyiri@student.moringaschool.com>
2021-07-18 15:32:48 +0300

    add funtion to check if info is valid

--- JavaScript/scripts.js
+++ JavaScript/scripts.js
@@ -28,3 +28,72 @@ function changeDOM() {
 
 
 }
+
+function validateForm() {
+    var gender = document.getElementsByName("gender");
+    var mdate = document.getElementById("mdate");
+    var mmonth = document.getElementById("mmonth");
+    var myear = document.getElementById("myear");
+    var formValid = false;
+    var i = 0;
+    document.getElementById("result").innerHTML = "";
+    if (mdate.value == "" || mdate.value == null) {
+
+        document.getElementById("result").innerHTML += "Day is Required";
+        return false;
+    } else {
+        mdate.style.border = "";
+        if (!isNaN(mdate.value)) {
+            if (mdate.value <= 0 || mdate.value > 31) {
+                document.getElementById("result").innerHTML += "Invalid Day";
+            }
+        } else {
+            document.getElementById("result").innerHTML += "Day must be a number";
+            return false;
+        }
+    }
+    if (mmonth.value == "" || mmonth.value == null) {
+        document.getElementById("result").innerHTML += "Month is Required";
+        return false;
+    } else {
+        mmonth.style.border = "";
+        if (!isNaN(mmonth.value)) {
+            if (mmonth.value <= 0 || mmonth.value > 12) {
+                document.getElementById("result").innerHTML += "Invalid Month";
+                return false;
+            }
+        } else {
+            document.getElementById("result").innerHTML += "Month must be a number";
+            return false;
+        }
+    }
+    if (myear.value == "" || myear.value == null) {
+        document.getElementById("result").innerHTML += "Year is Required";
+        return false;
+    } else {
+        myear.style.border = "";
+        if (!isNaN(myear.value)) {
+            if (myear.value.length != 4) {
+                document.getElementById("result").innerHTML += "Invalid Year";
+                return false;
+            }
+        } else {
+            document.getElementById("result").innerHTML += "Year must be a number";
+            return false;
+        }
+    }
+
+    while (!formValid && i < gender.length) {
+        if (gender[i].checked)
+            formValid = true;
+        i++;
+    }
+    if (!formValid) {
+        document.getElementById("the-gender").style.color = 'red';
+        return false;
+    }
+
+
+    return formValid;
+
+}
